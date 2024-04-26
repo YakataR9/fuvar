8d1c3fa0a447c61c391aadd353e3a204805522dc
YakataR9 <aranyos.zalan@gmail.com>
2024-04-26 11:33:28 +0200

    negyedik feladat

--- fuvar.py
+++ fuvar.py
@@ -5,7 +5,7 @@ class Fuvar:
         self.utazastartalma = utazastartalma
         self.tavolsag = tavolsag
         self.viteldij = viteldij
-        self.borravalo = borravalo
+        self.borravalo = float(borravalo)
         self.fizetesmod = fizetesmod
 
 fuvarlista = []
@@ -15,9 +15,24 @@ with open("fuvar.csv", "rt",  encoding="utf-8") as f:
     f.readline()
     for sor in f:
         sor = sor.strip().split(";")
+        sor[5] = sor[5].replace(",",".")
+        sor[4] = sor[4].replace(",",".")
         tmp = Fuvar(sor[0],sor[1],sor[2],sor[3],sor[4],sor[5],sor[6])
         fuvarlista.append(tmp)
         utazasfeljegyzesre += 1
         
 print("3. feladat: ",utazasfeljegyzesre,"fuvar")
-        
+
+fuvarszam = 0
+borravalok = 0
+
+for sor in fuvarlista:
+    if sor.taxiazonosito == "6185":
+        fuvarszam += 1
+        borravalok += float(sor.borravalo) + float(sor.viteldij)
+        print(borravalok)
+        if fuvarszam == 4:
+            print("4. feladat:",fuvarszam,"fuvar alatt:",borravalok,"$")
+
+
+                
